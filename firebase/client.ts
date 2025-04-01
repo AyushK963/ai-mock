0fa67bb658bb8d76f45625989ca0780354f830d3
Ayush Kankane <84469344+AyushK963@users.noreply.github.com>
2025-04-01 23:39:17 +0530

    Update client.ts

--- firebase/client.ts
+++ firebase/client.ts
@@ -4,13 +4,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA2IBfcG1WD-zQ1hZDXOa-_mt9W8c0QOJ0",
-    authDomain: "prepwise-4287a.firebaseapp.com",
-    projectId: "prepwise-4287a",
-    storageBucket: "prepwise-4287a.firebasestorage.app",
-    messagingSenderId: "718591450179",
-    appId: "1:718591450179:web:829d057d79dcd3a761c8f6",
-    measurementId: "G-RHTQ8XQHR5"
+  apiKey: "AIzaSyDwza8Pw2sblNg2ViUl-BxPOVhwFfUuT-I",
+  authDomain: "prepwise-f28d1.firebaseapp.com",
+  projectId: "prepwise-f28d1",
+  storageBucket: "prepwise-f28d1.firebasestorage.app",
+  messagingSenderId: "294006253048",
+  appId: "1:294006253048:web:8ee49e64be3ef0f103e20d",
+  measurementId: "G-GEKHT6HWKQ"
 };
 
 const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
